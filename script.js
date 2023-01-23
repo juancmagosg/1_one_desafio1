d117cc587a8b4d514fdc7ab1756353a2f4f45713
“JuanMagos” <juancmagosg17@gmail.com>
2023-01-23 16:27:44 -0600

    Se usa let en la variable

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function encriptar(){
-  var texto = document.getElementById("inputTexto").value.toLowerCase();
+  let texto = document.getElementById("inputTexto").value.toLowerCase();
   // i para revisar mayúsculas y minúsculas
   // g para toda la línea u oración
   // m para revisar múltiples líneas o párrafo
-  var txtCifrado = texto.replace(/e/igm,"enter");
+  let txtCifrado = texto.replace(/e/igm,"enter");
   // Sigue reemplazando sobre el texto que se empiza a revisar
-  var txtCifrado = txtCifrado.replace(/o/igm,"ober");
-  var txtCifrado = txtCifrado.replace(/i/igm,"imes");
-  var txtCifrado = txtCifrado.replace(/a/igm,"ai");
-  var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+  txtCifrado = txtCifrado.replace(/o/igm,"ober");
+  txtCifrado = txtCifrado.replace(/i/igm,"imes");
+  txtCifrado = txtCifrado.replace(/a/igm,"ai");
+  txtCifrado = txtCifrado.replace(/u/igm,"ufat");
 
   // Imagen y texto derechos se ocultan al encriptar
   document.getElementById("imgDer").style.display = "none";
@@ -22,16 +22,16 @@ function encriptar(){
 }
 
 function desencriptar(){
-  var texto = document.getElementById("inputTexto").value.toLowerCase();
+  let texto = document.getElementById("inputTexto").value.toLowerCase();
   // i para revisar mayúsculas y minúsculas
   // g para toda la línea u oración
   // m para revisar múltiples líneas o párrafo
-  var txtCifrado = texto.replace(/enter/igm,"e");
+  let txtCifrado = texto.replace(/enter/igm,"e");
   // Sigue reemplazando sobre el texto que se empiza a revisar
-  var txtCifrado = txtCifrado.replace(/ober/igm,"o");
-  var txtCifrado = txtCifrado.replace(/imes/igm,"i");
-  var txtCifrado = txtCifrado.replace(/ai/igm,"a");
-  var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+  txtCifrado = txtCifrado.replace(/ober/igm,"o");
+  txtCifrado = txtCifrado.replace(/imes/igm,"i");
+  txtCifrado = txtCifrado.replace(/ai/igm,"a");
+  txtCifrado = txtCifrado.replace(/ufat/igm,"u");
   
   document.getElementById("imgDer").style.display = "none";
   document.getElementById("texto").style.display = "none";
@@ -44,7 +44,7 @@ function desencriptar(){
 
 
 function copiar(){
-  var contenido = document.getElementById("texto2").innerHTML;    
+  let contenido = document.getElementById("texto2").innerHTML;    
   navigator.clipboard.writeText(contenido);
   alert("¡Se copió el texto!");
 }
